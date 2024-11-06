6eb275f402a0b93819e131d50e7589a6dc819ed2
WilliamsAl282 <WilliamsAl282@northwested.org>
2024-11-06 10:52:15 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+# Alexander Williams
+# 11/6/2024
+# Web and App development
+
+
+# Counting up and Down
+
+variable =int(input('Please enter an integer:\n'))
+#for number in range (1, (variable + 1)):
+ #   print(number)
+
+for number2 in range((variable),0, -1):
+    print(number2)
+print('We have lift off!')
