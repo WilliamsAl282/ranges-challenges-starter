f1c9f1c8dc20d979152fd03314b8430edc629e4b
WilliamsAl282 <WilliamsAl282@northwested.org>
2024-11-06 11:09:07 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,34 @@
 
 # Counting up and Down
 
-variable =int(input('Please enter an integer:\n'))
+#variable =int(input('Please enter an integer:\n'))
 #for number in range (1, (variable + 1)):
  #   print(number)
 
-for number2 in range((variable),0, -1):
-    print(number2)
-print('We have lift off!')
+# for number2 in range((variable),0, -1):
+#    print(number2)
+# print('We have lift off!')
+
+# Number Cubes
+
+#for number in range(1,11):
+ #   cubed = number ** 3
+ #   print(f'The cube of {number} is {int(cubed)}')
+
+# Multiplication Table
+
+#number = 7
+#choosen_num = int(input('choose a number: \n'))
+#for i in range(1)
+
+
+# List iteration
+
+total = 0
+numbers = [10,20,30,40,50,60,70,80,90,100]
+
+num_elements = len(numbers)
+
+for i in range(num_elements):
+    print(i)
+
